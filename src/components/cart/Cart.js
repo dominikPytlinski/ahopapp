63853c5f8c50e4758d8095b2ab9bcff727705f38
unknown <dominik_p@wp.pl>
2019-06-12 01:10:13 +0200

    Added scrollbar to Cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -11,28 +11,66 @@ class Cart extends React.Component {
                 <div className="cart-header">
                     <span onClick={this.props.closeCart}><FontAwesomeIcon icon="times" /></span>
                 </div>
-                <div className="cart-body">
-                    <div className="cart-item">
-                        <img src="img/sofa.jpg" alt="product"/>
-                        <span>$10</span>
-                        <div className="cart-item-counter">
-                            <span><FontAwesomeIcon icon="chevron-up" /></span>
-                            <span>1</span>
-                            <span><FontAwesomeIcon icon="chevron-down" /></span>
+                <div className="cart-content">
+                    <div className="cart-body">
+                        <div className="cart-item">
+                            <img src="img/sofa.jpg" alt="product"/>
+                            <span>$10</span>
+                            <div className="cart-item-counter">
+                                <span><FontAwesomeIcon icon="chevron-up" /></span>
+                                <span>1</span>
+                                <span><FontAwesomeIcon icon="chevron-down" /></span>
+                            </div>
+                        </div>
+                        <div className="cart-item">
+                            <img src="img/sofa.jpg" alt="product"/>
+                            <span>$10</span>
+                            <div className="cart-item-counter">
+                                <span><FontAwesomeIcon icon="chevron-up" /></span>
+                                <span>1</span>
+                                <span><FontAwesomeIcon icon="chevron-down" /></span>
+                            </div>
+                        </div>
+                        <div className="cart-item">
+                            <img src="img/sofa.jpg" alt="product"/>
+                            <span>$10</span>
+                            <div className="cart-item-counter">
+                                <span><FontAwesomeIcon icon="chevron-up" /></span>
+                                <span>1</span>
+                                <span><FontAwesomeIcon icon="chevron-down" /></span>
+                            </div>
+                        </div>
+                        <div className="cart-item">
+                            <img src="img/sofa.jpg" alt="product"/>
+                            <span>$10</span>
+                            <div className="cart-item-counter">
+                                <span><FontAwesomeIcon icon="chevron-up" /></span>
+                                <span>1</span>
+                                <span><FontAwesomeIcon icon="chevron-down" /></span>
+                            </div>
+                        </div>
+                        <div className="cart-item">
+                            <img src="img/sofa.jpg" alt="product"/>
+                            <span>$10</span>
+                            <div className="cart-item-counter">
+                                <span><FontAwesomeIcon icon="chevron-up" /></span>
+                                <span>1</span>
+                                <span><FontAwesomeIcon icon="chevron-down" /></span>
+                            </div>
+                        </div>
+                        <div className="cart-item">
+                            <img src="img/sofa.jpg" alt="product"/>
+                            <span>$10</span>
+                            <div className="cart-item-counter">
+                                <span><FontAwesomeIcon icon="chevron-up" /></span>
+                                <span>1</span>
+                                <span><FontAwesomeIcon icon="chevron-down" /></span>
+                            </div>
                         </div>
                     </div>
-                    <div className="cart-item">
-                        <img src="img/sofa.jpg" alt="product"/>
-                        <span>$10</span>
-                        <div className="cart-item-counter">
-                            <span><FontAwesomeIcon icon="chevron-up" /></span>
-                            <span>1</span>
-                            <span><FontAwesomeIcon icon="chevron-down" /></span>
-                        </div>
+                    <div className="cart-footer">
+                        <span>Total: $10</span>
                     </div>
-                </div>
-                <div className="cart-footer">
-                    <span>Total: $10</span>
                 </div>
             </div>
         );
