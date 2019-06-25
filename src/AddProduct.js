65908be05f3de29ec868fd9167e3bbe839d4a57c
Dominik <dominik_p@wp.pl>
2019-06-25 21:18:53 +0200

    Created AddProduct component

---
+++ src/AddProduct.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { getFromSessionStorage } from './modules/Services';
+import './AddProduct.css';
+
+class AddProduct extends React.Component {
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            isLogged: (getFromSessionStorage('jwt')) ? true : false,
+            description: '',
+            price: '',
+            data: ''
+        }
+    }
+
+    setDescription = (e) => {
+        this.setState({ description: e.target.value });
+    }
+
+    setPrice = (e) => {
+        this.setState({ price: e.target.value });
+    }
+    
+    render()
+    {
+        const { description, price, isLogged } = this.state;
+
+        return(
+            <div className="add-product">
+                {(isLogged === true) ? null : <Redirect to="/login" /> }
+                <header>
+                    <span>Comphy Ferniture</span>
+                </header>
+                <div className="form-container">
+                    <form className="form">
+                       <label>
+                            Opis:<input type="text" value={description} onChange={this.setDescription} />   
+                        </label>
+                        <label>
+                            Cena:<input type="number" value={price} onChange={this.setPrice} />   
+                        </label>
+                        <label>
+                            Dodaj zdjęcie:<input type="file" />   
+                        </label>
+                        <input type="submit" value="Zapisz" />
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default AddProduct;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Login from './Login';
+import AddProduct from './AddProduct';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render((
     <BrowserRouter>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
+        <Route path="/add-product" component={AddProduct} />
     </BrowserRouter>
 ), document.getElementById('root'));
 
